8933b3edef00c70b748584d6b5d48eb3f9190cd8
yachea <dev.yachea@gmail.com>
2025-10-17 13:19:22 +0900

    [docs] shadcn 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import ButtonTest from '@/components/ButtonTest';
 import TestComponent from '@/components/TestComponent';
 
 export default function Home() {
   return (
     <div>
-      <TestComponent title='안녕하세요.' description='SCSS 테스트입니다.' />
+      {/* <TestComponent title='안녕하세요.' description='SCSS 테스트입니다.' /> */}
+      <ButtonTest />
     </div>
   );
 }

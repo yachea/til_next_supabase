85dce53aebff1f80dfc47b4c606308407471340a
yachea <dev.yachea@gmail.com>
2025-10-27 13:07:52 +0900

    [docs] 이메일 회원 가입

---
+++ src/apis/auth.ts
@@ -0,0 +1,15 @@
+import supabase from '@/lib/supabase/client';
+
+// Supabase 백엔드에 사용자 이메일 회원가입
+export async function signUpWithEmail({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  // 웹브라우저를 이용해서 이메일 회원가입
+  const { data, error } = await supabase.auth.signUp({ email, password });
+  if (error) throw error;
+  return data;
+}

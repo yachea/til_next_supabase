6cb3f4158df04d3da557db5744582c61043fa251
yachea <dev.yachea@gmail.com>
2025-10-31 11:01:48 +0900

    [docs] zustand 인증 보관 적용

---
+++ src/components/providers/SessionProvider.tsx
@@ -0,0 +1,32 @@
+'use client';
+import supabase from '@/lib/supabase/client';
+import { useSessionLoaded, useSetSession } from '@/stores/session';
+import React, { useEffect } from 'react';
+import { GlobalLoading } from '../GlobalLoading';
+
+interface SessionProviderProps {
+  children: React.ReactNode;
+}
+
+export default function SessionProvider({ children }: SessionProviderProps) {
+  const setSession = useSetSession();
+  const isSessionLoaded = useSessionLoaded();
+
+  useEffect(() => {
+    // Supabase 의 인증의 상태가 변함을 체크함.
+    supabase.auth.onAuthStateChange((event, session) => {
+      // zustand 에 보관
+      setSession(session);
+    });
+  }, []);
+
+  // 아직 세션이 없다면
+  if (!isSessionLoaded)
+    return (
+      <div>
+        <GlobalLoading />
+      </div>
+    );
+
+  return <div>{children}</div>;
+}
